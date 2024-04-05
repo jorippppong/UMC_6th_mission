ba7d01eae31abd9d000e44def37c1e3f2e8d53ac
jorippppong <duelee75@gmail.com>
2024-04-05 15:44:24 +0900

    first commit

---
+++ src/main/java/com/forUMC/Umc6thMissionApplication.java
@@ -0,0 +1,13 @@
+package com.forUMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc6thMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Umc6thMissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forUMC/Umc6thMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forUMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc6thMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
