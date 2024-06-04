b2cf09ef5dac4519f147785d727e985bc104fb66
jorippppong <duelee75@gmail.com>
2024-06-04 22:18:19 +0900

    [FIX #13] db 설정

--- src/main/java/com/forUMC/app/web/controller/HealthController.java
+++ src/main/java/com/forUMC/app/web/controller/HealthController.java
@@ -9,6 +9,6 @@ public class HealthController {
 
     @GetMapping("/health")
     public ResponseEntity<String> health(){
-        return ResponseEntity.ok("healthy!");
+        return ResponseEntity.ok("healthy!!!");
     }
 }
